2490b4c076ccfb6c615df37f9b94b353011358af
indiademo <abhishek03094@gmail.com>
2019-02-24 02:14:34 +0530

    latest

--- server.js
+++ server.js
@@ -1,9 +1,36 @@
-import { default as express} from './config/express';
+exp=require("express")
+bp=require("body-parser")
 
-const app = express();
+jst=require("jsonwebtoken")
+bc=require("bcrypt")
 
-app.listen( 2000, ()=>{
-    console.log("Server listening to port:", 2000);
-})
+//app.use()
+app=exp()
+app.use(bp.json())
+sec=require("./myfiles/encript")
 
-export {app};
+catfile=require("./myfiles/cat_server")
+subcatfile=require("./myfiles/sub_cat")
+sscat=require("./myfiles/sub_subcat")
+pro=require("./myfiles/product")
+userd=require("./myfiles/userlog")
+orde=require("./myfiles/orders")
+pay=require("./myfiles/payment")
+
+app.use(exp.static('src/assets/images'))
+
+
+
+app.use(exp.static("dist"))
+
+app.use("/catser",catfile)
+app.use("/subcatser",subcatfile)
+app.use("/subsubcatser",sscat)
+app.use("/productser",pro)
+app.use("/userser",userd)
+app.use("/userorders",orde)
+app.use("/payment",pay)
+app.listen(2244)
+console.log("2244")
+
+
