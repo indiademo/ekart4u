1b80e78a3fb3f382a5826e42113368faf9021d67
indiademo <abhishek03094@gmail.com>
2019-02-24 13:23:19 +0530

    ok

--- server.js
+++ server.js
@@ -1,36 +1,18 @@
-exp=require("express")
-bp=require("body-parser")
-
-jst=require("jsonwebtoken")
-bc=require("bcrypt")
+var exp=require("express")
+var bp=require("body-parser")
 
 var PORT = process.env.PORT || 5000
-//app.use()
+var path = require('path');
 app=exp()
 app.use(bp.json())
-sec=require("./myfiles/encript")
-
-catfile=require("./myfiles/cat_server")
-subcatfile=require("./myfiles/sub_cat")
-sscat=require("./myfiles/sub_subcat")
-pro=require("./myfiles/product")
-userd=require("./myfiles/userlog")
-orde=require("./myfiles/orders")
-pay=require("./myfiles/payment")
-
-app.use(exp.static('src/assets/images'))
 
 
 
-app.use(exp.static("dist"))
 
-app.use("/catser",catfile)
-app.use("/subcatser",subcatfile)
-app.use("/subsubcatser",sscat)
-app.use("/productser",pro)
-app.use("/userser",userd)
-app.use("/userorders",orde)
-app.use("/payment",pay)
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
 app.listen(PORT,function(){
     console.log("2244")
 })
